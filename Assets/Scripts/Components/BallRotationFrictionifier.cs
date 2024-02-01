fa32a2ec961711193249cd3f2cac9c5f4e6f49a1
Lars Mitsem Selbekk <lars@selbekk.com>
2024-02-01 16:44:14 +0100

    feat: implement ball spin

---
+++ Assets/Scripts/Components/BallRotationFrictionifier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using Utils;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallRotationFrictionifier : MonoBehaviour
+{
+    public float scaleFactor = 0.005f;
+    public float minAngularVelocity = 0.01f;
+    public float minVelocity = 0.01f;
+
+    private Rigidbody _rigidbody;
+
+    private void OnEnable()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void FixedUpdate()
+    {
+        // TODO: handle scaling
+        if (_rigidbody.angularVelocity.magnitude > minAngularVelocity && _rigidbody.velocity.magnitude > minVelocity)
+        {
+            _rigidbody.AddForce((Vector3.Cross(_rigidbody.velocity, _rigidbody.angularVelocity) * scaleFactor).AlignedToXZPlane());
+        }
+    }
+}
