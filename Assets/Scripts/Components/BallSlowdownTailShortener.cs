d471b31e1453065d0c97de4b4f2d5de621bbd21a
Mathias Oterhals Myklebust <mathiom@stud.ntnu.no>
2024-03-05 12:24:57 +0100

    feat(BallSlowdownTailShortener): remove angular velocity if below threshold

--- Assets/Scripts/Components/BallSlowdownTailShortener.cs
+++ Assets/Scripts/Components/BallSlowdownTailShortener.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class BallSlowdownTailShortener : MonoBehaviour
 {
-    public float slowdownAngularSpeedThreshold = 1f;
-    public float slowdownPerSecond = 1f;
+    public float comeToRestAngularSpeedThreshold = 0.01f;
+    public float slowdownAngularSpeedThreshold = 8f;
+    public float slowdownPerSecond = 1.75f;
     private Rigidbody _rigidbody;
 
     private void OnEnable()
@@ -14,6 +15,11 @@ public class BallSlowdownTailShortener : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (_rigidbody.angularVelocity.magnitude < comeToRestAngularSpeedThreshold)
+        {
+            _rigidbody.angularVelocity = Vector3.zero;
+            return;
+        }
         if (_rigidbody.angularVelocity.magnitude < slowdownAngularSpeedThreshold)
         {
             _rigidbody.AddTorque(-_rigidbody.angularVelocity * (slowdownPerSecond * Time.fixedDeltaTime), ForceMode
